a0d610bb2b8448b85154925d91e5bfb98384abb4
jasmeen0001 <jasmeensohi777@gmail.com>
2024-05-03 22:43:43 +0530

    updated

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "bd2857a208448264ab3cfeeae6aa766c",
-"index.html": "26ef05e4a1c81b2897d763598ca9287e",
-"/": "26ef05e4a1c81b2897d763598ca9287e",
-"main.dart.js": "a5f0256b733f973507ecea830e0c8566",
+"index.html": "b1384cf0f3fbf43b266bf26b8c2e394f",
+"/": "b1384cf0f3fbf43b266bf26b8c2e394f",
+"main.dart.js": "fd015dfb5b72fd9937f45a9ba2f569fb",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
